f5cf7f51ea115e28a276721c1a92250e65613a51
SeungukSon <seunguksondev@gmail.com>
2019-10-17 02:00:38 +0900

    Code_Addition:Visualization
    Pyplot을 이용한 시각화 코드를 추가합니다. 
    PCA(components=2)를 이용해 50d를 2d상에 표현하였습니다. 
    코드를 수정하면  train/test에 대한 plot도 얻을 수 있습니다.

--- read_data_cifar10.py
+++ read_data_cifar10.py
@@ -3,7 +3,11 @@ import _pickle as cPickle
 import word_vec
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import normalize
-
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+import regression_based
+from numpy import array
+from word_vec import get_vectors
 def unpickle(file):
     fo = open(file, 'rb')
     dict = cPickle.load(fo, encoding='latin1')
@@ -148,7 +152,7 @@ def regression_embedding():
 
     targets_embeddings = []
     # for i in class_labels:
-    for i in ['automobile', 'deer']:
+    for i in ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse','ship', 'truck']:
         targets_embeddings.append(embeddings[i])
     targets_embeddings = np.array(targets_embeddings, dtype=np.float32)
 
@@ -164,10 +168,10 @@ def regression_embedding():
     #     Y_pred_validation.append(np.argmax(cos))
     #
     neigh = KNeighborsClassifier(n_neighbors=1)
-    neigh.fit(targets_embeddings, [1,4])
+    neigh.fit(targets_embeddings, [0,1,2,3,4,5,6,7,8,9])
     Y_pred_validation = neigh.predict(validaiton_embeddings)
-    # print Y_2_validation
-    # print Y_pred_validation
+    print(Y_2_validation)
+    print(Y_pred_validation)
     print (accuracy_score(Y_2_validation, Y_pred_validation))
 
 
@@ -186,3 +190,56 @@ def regression_embedding():
 
 regression_embedding()
 # classify_embedding()
+
+vec = get_vectors()
+
+Y_8_train = np.array(Y_train)
+X_8_train = np.array(X_train)
+
+removed_indices = np.where(Y_8_train!=1)
+Y_8_train = Y_8_train[removed_indices]
+X_8_train = X_8_train[removed_indices]
+removed_indices = np.where(Y_8_train!=4)
+Y_8_train = Y_8_train[removed_indices]
+X_8_train = X_8_train[removed_indices]
+
+Y_2_validation = np.array(Y)
+X_2_validation = np.array(X)
+
+indices = np.where(np.logical_or(Y_2_validation == 1, Y_2_validation == 4))
+Y_2_validation = Y_2_validation[indices]
+X_2_validation = X_2_validation[indices]
+
+classes = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse','ship', 'truck']
+
+class_list = [vec[i] for i in classes]
+# print(class_list)
+a = regression_based.predict(np.array(X_train)[:20000])  # 예측부
+
+pca = PCA(2)
+class_list.extend(a)
+total_list = pca.fit_transform(class_list)
+
+tx = [row for row, col in total_list[:10]]
+ty = [col for row, col in total_list[:10]]
+x_t=[]
+y_t=[]
+from matplotlib.font_manager import FontProperties
+for j in range(10):
+    x_t.append([row for i,(row, col) in enumerate(total_list[10:]) if Y_train[i] == j])  #위의 예측부를 바꿔줬다면 여기도 수정해야함 ex) X_train->X_8_train으로 바궛다면 Y_8_train[i]로
+    y_t.append([col for i, (row, col) in enumerate(total_list[10:]) if Y_train[i] == j]) #위의 예측부를 바꿔줬다면 여기도 수정해야함
+
+marker = ['.','o','v','^','>','<','s','p','*','h']   #마커랑 컬러 바꾸고싶다면..여기를 수정
+colors = ['g','b','c','y','m','g','b','c','y','m']
+for j in range(10):
+    plt.scatter(x_t[j],y_t[j],marker=marker[j],color = colors[j],label=classes[j])
+fontP = FontProperties()
+fontP.set_size('small')
+plt.legend(bbox_to_anchor=(0,1), loc='upper right',prop=fontP)
+# plt.scatter(x_1,y_1,color='b')
+# plt.scatter(x_4,y_4, color = 'y')
+plt.scatter(tx,ty,color='r')
+for i, txt in enumerate(classes):
+    plt.annotate(txt,(tx[i],ty[i]))
+
+plt.show()
